ea127796ef97362edbf3a023a684a336f94ed602
Nazarii Muzychuk <muzychuk.n@gmail.com>
2020-03-29 15:46:58 +0300

    Add jms resource

--- src/main/java/com/nmuzychuk/txdemo/MessageService.java
+++ src/main/java/com/nmuzychuk/txdemo/MessageService.java
@@ -1,11 +1,12 @@
 package com.nmuzychuk.txdemo;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
-import static com.nmuzychuk.txdemo.QueryHelper.*;
+import static com.nmuzychuk.txdemo.MessageServiceHelper.*;
 
 @Service
 @Transactional
@@ -13,25 +14,42 @@ public class MessageService {
 
     private final JdbcTemplate messageJdbcTemplate;
     private final JdbcTemplate auditJdbcTemplate;
+    private final JmsTemplate jmsTemplate;
 
-    public MessageService(JdbcTemplate messageJdbcTemplate, JdbcTemplate auditJdbcTemplate) {
+    public MessageService(JdbcTemplate messageJdbcTemplate, JdbcTemplate auditJdbcTemplate, JmsTemplate jmsTemplate) {
         this.messageJdbcTemplate = messageJdbcTemplate;
         this.auditJdbcTemplate = auditJdbcTemplate;
+        this.jmsTemplate = jmsTemplate;
     }
 
     public void saveMessage() {
         messageJdbcTemplate.execute(INSERT_MESSAGE_SQL);
         auditJdbcTemplate.execute(INSERT_AUDIT_SQL);
+        jmsTemplate.convertAndSend(MESSAGE_QUEUE_NAME, MESSAGE_BODY);
     }
 
     public void saveMessageFail() {
         messageJdbcTemplate.execute(INSERT_MESSAGE_FAIL_SQL);
         auditJdbcTemplate.execute(INSERT_AUDIT_SQL);
+        jmsTemplate.convertAndSend(MESSAGE_QUEUE_NAME, MESSAGE_BODY);
     }
 
     public void saveMessageAuditFail() {
         messageJdbcTemplate.execute(INSERT_MESSAGE_SQL);
         auditJdbcTemplate.execute(INSERT_AUDIT_FAIL_SQL);
+        jmsTemplate.convertAndSend(MESSAGE_QUEUE_NAME, MESSAGE_BODY);
+    }
+
+    public void saveMessageAuditFailAfterJms() {
+        messageJdbcTemplate.execute(INSERT_MESSAGE_SQL);
+        jmsTemplate.convertAndSend(MESSAGE_QUEUE_NAME, MESSAGE_BODY);
+        auditJdbcTemplate.execute(INSERT_AUDIT_FAIL_SQL);
+    }
+
+    public void saveMessageJmsFail() {
+        messageJdbcTemplate.execute(INSERT_MESSAGE_SQL);
+        auditJdbcTemplate.execute(INSERT_AUDIT_SQL);
+        jmsTemplate.convertAndSend(MESSAGE_QUEUE_NAME, MESSAGE_BODY, null);
     }
 
 }
--- src/test/java/com/nmuzychuk/txdemo/TxDemoApplicationTests.java
+++ src/test/java/com/nmuzychuk/txdemo/TxDemoApplicationTests.java
@@ -5,8 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jms.core.JmsTemplate;
 
-import static com.nmuzychuk.txdemo.QueryHelper.*;
+import java.util.Collections;
+
+import static com.nmuzychuk.txdemo.MessageServiceHelper.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -20,6 +23,9 @@ class TxDemoApplicationTests {
 
     @Autowired
     MessageService messageService;
+
+    @Autowired
+    JmsTemplate jmsTemplate;
 
     @Test
     void contextLoads() {
@@ -53,48 +59,101 @@ class TxDemoApplicationTests {
     void messageServiceSaveMessage() {
         Integer beforeMessageCount = messageJdbcTemplate.queryForObject(MESSAGE_COUNT_SQL, Integer.class);
         Integer beforeAuditCount = auditJdbcTemplate.queryForObject(AUDIT_COUNT_SQL, Integer.class);
+        Integer beforeMessageQueueSize = messageQueueSize();
 
         messageService.saveMessage();
 
         Integer afterMessageCount = messageJdbcTemplate.queryForObject(MESSAGE_COUNT_SQL, Integer.class);
         Integer afterAuditCount = auditJdbcTemplate.queryForObject(AUDIT_COUNT_SQL, Integer.class);
+        Integer afterMessageQueueSize = messageQueueSize();
 
         assertNotNull(beforeMessageCount);
         assertNotNull(beforeAuditCount);
         assertEquals(beforeMessageCount + 1, afterMessageCount);
         assertEquals(beforeAuditCount + 1, afterAuditCount);
+        assertEquals(beforeMessageQueueSize + 1, afterMessageQueueSize);
     }
 
     @Test
     void messageServiceSaveMessageFail() {
         Integer beforeMessageCount = messageJdbcTemplate.queryForObject(MESSAGE_COUNT_SQL, Integer.class);
         Integer beforeAuditCount = auditJdbcTemplate.queryForObject(AUDIT_COUNT_SQL, Integer.class);
+        Integer beforeMessageQueueSize = messageQueueSize();
 
         assertThrows(BadSqlGrammarException.class, () -> messageService.saveMessageFail());
 
         Integer afterMessageCount = messageJdbcTemplate.queryForObject(MESSAGE_COUNT_SQL, Integer.class);
         Integer afterAuditCount = auditJdbcTemplate.queryForObject(AUDIT_COUNT_SQL, Integer.class);
+        Integer afterMessageQueueSize = messageQueueSize();
 
         assertNotNull(beforeMessageCount);
         assertNotNull(beforeAuditCount);
         assertEquals(beforeMessageCount, afterMessageCount);
         assertEquals(beforeAuditCount, afterAuditCount);
+        assertEquals(beforeMessageQueueSize, afterMessageQueueSize);
     }
 
     @Test
     void messageServiceSaveMessageAuditFail() {
         Integer beforeMessageCount = messageJdbcTemplate.queryForObject(MESSAGE_COUNT_SQL, Integer.class);
         Integer beforeAuditCount = auditJdbcTemplate.queryForObject(AUDIT_COUNT_SQL, Integer.class);
+        Integer beforeMessageQueueSize = messageQueueSize();
 
         assertThrows(BadSqlGrammarException.class, () -> messageService.saveMessageAuditFail());
 
         Integer afterMessageCount = messageJdbcTemplate.queryForObject(MESSAGE_COUNT_SQL, Integer.class);
         Integer afterAuditCount = auditJdbcTemplate.queryForObject(AUDIT_COUNT_SQL, Integer.class);
+        Integer afterMessageQueueSize = messageQueueSize();
 
         assertNotNull(beforeMessageCount);
         assertNotNull(beforeAuditCount);
         assertEquals(beforeMessageCount, afterMessageCount);
         assertEquals(beforeAuditCount, afterAuditCount);
+        assertEquals(beforeMessageQueueSize, afterMessageQueueSize);
+    }
+
+    @Test
+    void messageServiceSaveMessageAuditFailAfterJms() {
+        Integer beforeMessageCount = messageJdbcTemplate.queryForObject(MESSAGE_COUNT_SQL, Integer.class);
+        Integer beforeAuditCount = auditJdbcTemplate.queryForObject(AUDIT_COUNT_SQL, Integer.class);
+        Integer beforeMessageQueueSize = messageQueueSize();
+
+        assertThrows(BadSqlGrammarException.class, () -> messageService.saveMessageAuditFailAfterJms());
+
+        Integer afterMessageCount = messageJdbcTemplate.queryForObject(MESSAGE_COUNT_SQL, Integer.class);
+        Integer afterAuditCount = auditJdbcTemplate.queryForObject(AUDIT_COUNT_SQL, Integer.class);
+        Integer afterMessageQueueSize = messageQueueSize();
+
+        assertNotNull(beforeMessageCount);
+        assertNotNull(beforeAuditCount);
+        assertEquals(beforeMessageCount, afterMessageCount);
+        assertEquals(beforeAuditCount, afterAuditCount);
+        assertEquals(beforeMessageQueueSize, afterMessageQueueSize);
+    }
+
+    @Test
+    void messageServiceSaveMessageJmsFail() {
+        Integer beforeMessageCount = messageJdbcTemplate.queryForObject(MESSAGE_COUNT_SQL, Integer.class);
+        Integer beforeAuditCount = auditJdbcTemplate.queryForObject(AUDIT_COUNT_SQL, Integer.class);
+        Integer beforeMessageQueueSize = messageQueueSize();
+
+        assertThrows(NullPointerException.class, () -> messageService.saveMessageJmsFail());
+
+        Integer afterMessageCount = messageJdbcTemplate.queryForObject(MESSAGE_COUNT_SQL, Integer.class);
+        Integer afterAuditCount = auditJdbcTemplate.queryForObject(AUDIT_COUNT_SQL, Integer.class);
+        Integer afterMessageQueueSize = messageQueueSize();
+
+        assertNotNull(beforeMessageCount);
+        assertNotNull(beforeAuditCount);
+        assertEquals(beforeMessageCount, afterMessageCount);
+        assertEquals(beforeAuditCount, afterAuditCount);
+        assertEquals(beforeMessageQueueSize, afterMessageQueueSize);
+    }
+
+    private Integer messageQueueSize() {
+        return jmsTemplate.browse("messages", (
+                (session, browser) -> Collections.list(browser.getEnumeration()).size())
+        );
     }
 
 }
